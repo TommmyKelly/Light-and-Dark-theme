37aba12ba123fc38e8ddd22b1b711cd066ac513b
TommmyKelly <tommy_kelly@icloud.com>
2021-03-27 10:40:52 +0000

    Starter Pack

---
+++ src/components/Dashboard.js
@@ -0,0 +1,55 @@
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className='grid__container'>
+      <div className='grid__wrapper'>
+        <div className='item grid__heading'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet
+          nec ligula eget commodo. Aenean nec turpis elit. Sed sed tellus
+          viverra, tempus elit vitae, luctus ipsum. Maecenas eleifend vestibulum
+          tincidunt. Suspendisse ipsum leo, ultrices sed cursus ac, molestie sit
+          amet urna. Ut vel diam tempus, malesuada massa malesuada, auctor.
+        </div>
+        <div className='item grid__left'>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet
+          nec ligula eget commodo. Aenean nec turpis elit. Sed sed tellus
+          viverra, tempus elit vitae, luctus ipsum. Maecenas eleifend vestibulum
+          tincidunt. Suspendisse ipsum leo, ultrices sed cursus ac, molestie sit
+          amet urna. Ut vel diam tempus, malesuada massa malesuada, auctor.
+        </div>
+        <div className='item grid__right'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet
+          nec ligula eget commodo. Aenean nec turpis elit. Sed sed tellus
+          viverra, tempus elit vitae, luctus ipsum. Maecenas eleifend vestibulum
+          tincidunt. Suspendisse ipsum leo, ultrices sed cursus ac, molestie sit
+          amet urna. Ut vel diam tempus, malesuada massa malesuada, auctor.
+        </div>
+        <div className='item grid__side'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet
+          nec ligula eget commodo. Aenean nec turpis elit. Sed sed tellus
+          viverra, tempus elit vitae, luctus ipsum. Maecenas eleifend vestibulum
+          tincidunt. Suspendisse ipsum leo, ultrices sed cursus ac, molestie sit
+          amet urna. Ut vel diam tempus, malesuada massa malesuada, auctor.
+        </div>
+        <div className='item grid__side__right'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet
+          nec ligula eget commodo. Aenean nec turpis elit. Sed sed tellus
+          viverra, tempus elit vitae, luctus ipsum. Maecenas eleifend vestibulum
+          tincidunt. Suspendisse ipsum leo, ultrices sed cursus ac, molestie sit
+          amet urna. Ut vel diam tempus, malesuada massa malesuada, auctor.
+        </div>
+        <div className='item grid__side__right'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet
+          nec ligula eget commodo. Aenean nec turpis elit. Sed sed tellus
+          viverra, tempus elit vitae, luctus ipsum. Maecenas eleifend vestibulum
+          tincidunt. Suspendisse ipsum leo, ultrices sed cursus ac, molestie sit
+          amet urna. Ut vel diam tempus, malesuada massa malesuada, auctor.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
